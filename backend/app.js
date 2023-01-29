0d0705930629c18ae32ad7b957f37723b1a1494f
Daggim 99 <programmerdaggim99@gmail.com>
2023-01-30 01:36:39 +0300

    changes

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== "PRODUCTION")
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
 
